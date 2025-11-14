9886b7dde4f00668612dd2d972c9fdffa1cf9d5f
moonstoneG <76591064+moonstoneG@users.noreply.github.com>
2025-11-14 17:21:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -509,7 +509,7 @@ def generate_doc4(
         qty_converted = convert_qty(unit, qty_val)
         payload_items.append(ItemPricing(name, "条", qty_converted, float(price)))
     payload_items = merge_items(payload_items)
-    payload = Payload(
+    payload = PayloadPricing(
         bureau=bureau,
         suspect=" ",
         behavior=" ",
